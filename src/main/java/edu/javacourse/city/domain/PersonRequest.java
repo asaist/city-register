de2f1061a1d2171d4c09bb85fc667f9f09a0e89b
Vladimir <kosakvova@gmail.com>
2022-12-22 10:41:24 +0600

    123

--- src/main/java/edu/javacourse/city/domain/PersonRequest.java
+++ src/main/java/edu/javacourse/city/domain/PersonRequest.java
@@ -1,2 +1,20 @@
-package edu.javacourse.city.domain;public class PersonRequest {
+package edu.javacourse.city.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+import java.time.LocalDate;
+
+@Getter
+@Setter
+public class PersonRequest {
+
+    private String surName;
+    private String givenName;
+    private String patronymic;
+    private LocalDate dateOfBirth;
+    private int streetCode;
+    private String building;
+    private String extension;
+    private String apartment;
+
 }
--- src/main/java/edu/javacourse/city/domain/PersonResponse.java
+++ src/main/java/edu/javacourse/city/domain/PersonResponse.java
@@ -1,2 +1,22 @@
-package edu.javacourse.city.domain;public class PersonResponse {
+package edu.javacourse.city.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.beans.ConstructorProperties;
+
+@Getter
+@Setter
+
+public class PersonResponse {
+    private boolean registered;
+    private boolean temporal;
+
+    public PersonResponse() {
+    }
+
+    public PersonResponse(boolean registered, boolean temporal) {
+        this.registered = registered;
+        this.temporal = temporal;
+    }
 }
